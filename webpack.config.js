4848a76598202bde44ec97f0d4d08fe7eb9477af
comoco <como65416@gmail.com>
2019-01-01 15:04:30 +0800

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+  mode: 'development',
+  target: 'electron-renderer',
+  entry: [
+    './webpack-main.js'
+  ],
+  output: {
+    filename: 'bundle.js',
+    chunkFilename: '[name].bundle.js',
+    path: path.resolve(__dirname, './public/dist'),
+    publicPath: '/dist/'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    publicPath: '/dist/'
+  },
+  module: {
+    rules: [
+      {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader'
+      },
+      {
+        test: /\.vue$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ['vue-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=50000'
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `bundle.css`,
+    }),
+    new VueLoaderPlugin()
+  ]
+};
---
+++ webpack.prod.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { VueLoaderPlugin } = require('vue-loader');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+  mode: 'production',
+  target: 'electron-renderer',
+  entry: [
+    './webpack-main.js'
+  ],
+  output: {
+    filename: 'bundle.js',
+    chunkFilename: '[name].bundle.js',
+    path: path.resolve(__dirname, './public/dist'),
+    publicPath: '/dist/'
+  },
+  module: {
+    rules: [
+      {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader'
+      },
+      {
+        test: /\.vue$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ['vue-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=50000'
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          warnings: false,
+          parse: {},
+          compress: {},
+          mangle: true,
+          output: null,
+          toplevel: false,
+          nameCache: null,
+          ie8: false,
+          keep_fnames: false,
+        }
+      }),
+      new OptimizeCSSAssetsPlugin
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `bundle.css`,
+    }),
+    new VueLoaderPlugin()
+  ]
+};
