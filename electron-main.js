b581a5765ed72e75e2619a8ab66e8431e9d0e2a1
comoco <como65416@gmail.com>
2019-01-01 23:37:00 +0800

    add electron-main.js

---
+++ electron-main.js
@@ -0,0 +1,65 @@
+const { app, BrowserWindow, Menu, MenuItem} = require('electron')
+
+let win
+
+function createWindow() {
+  let indexPageURL = `file://${__dirname}/public/index.html`;
+  let windowConfig = {
+    width: 600,
+    height: 500,
+    icon: __dirname + '/icon.png'
+  };
+
+  const menuTemplate = [
+  {
+    label: 'Dn Tool Packager',
+    submenu: [
+      {role: 'quit'}
+    ]
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      {role: 'undo'},
+      {role: 'redo'},
+      {role: 'cut'},
+      {role: 'copy'},
+      {role: 'paste'},
+      {role: 'delete'},
+      {role: 'selectall'}
+    ]
+  }];
+
+  if (process.argv.includes('--dev')) {
+    indexPageURL = `http://localhost:8089/index.html`;
+    windowConfig.webPreferences = {
+      webSecurity: false
+    }
+    menuTemplate.push({
+      label: 'Development',
+      submenu: [
+        {role: 'reload'},
+        {role: 'forcereload'},
+        {role: 'toggledevtools'}
+      ]
+    });
+  }
+
+  win = new BrowserWindow(windowConfig);
+  win.loadURL(indexPageURL);
+
+  const applicationMenu = Menu.buildFromTemplate(menuTemplate)
+  Menu.setApplicationMenu(applicationMenu)
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
